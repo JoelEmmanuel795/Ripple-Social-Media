86180b7293feede7a6b36bc94773da67067b1c1f
giavaphi <giavaphi@students.zhaw.ch>
2025-06-02 14:51:18 +0200

    fix refresh error

--- src/layouts/ProtectedLayout.jsx
+++ src/layouts/ProtectedLayout.jsx
@@ -1,18 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router';
 import axios from 'axios';
 import { login_user, logout_user } from '../store/slices/userSlice';
 
 const ProtectedLayout = () => {
+    const [validated, setValidated] = useState(false);
     const token = useSelector((state) => state.user.accessToken);
     const dispatch = useDispatch();
 
     // Validate the token
     useEffect(() => {
         const fetch = async () => {
+            setValidated(false);
             const accessToken = localStorage.getItem('access');
-            console.log(accessToken);
 
             if (accessToken) {
                 const api =
@@ -21,21 +22,19 @@ const ProtectedLayout = () => {
                     await axios.post(api, {
                         token: accessToken,
                     });
-                    console.log('token valid');
                     dispatch(login_user(accessToken));
                 } catch {
-                    console.log('Logout error');
                     dispatch(logout_user());
+                    setValidated(true);
                 }
             } else {
-                console.log('Logout 1');
                 dispatch(logout_user());
             }
         };
         fetch();
     }, [dispatch]);
 
-    if (!token) {
+    if (!token && validated) {
         return <Navigate to="/auth/login" replace />;
     }
 
