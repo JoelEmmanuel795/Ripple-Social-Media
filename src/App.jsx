edb278886744a04f2adfb5b5261b26102b746224
jithin <jithinpeechatt@gmail.com>
2025-06-02 13:47:44 +0200

    commented out unused var

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,7 @@ function App() {
         <>
             <Sidebar />
             <Login />
-            // <FriendCard friend={mockFriend} />
-
+            {/* <FriendCard friend={mockFriend} /> */}
         </>
     );
 }
