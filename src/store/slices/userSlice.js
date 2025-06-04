2dd52ed7eae3bd1692727d7e1fd0f02f48cc96b7
jithin <jithinpeechatt@gmail.com>
2025-06-04 13:00:49 +0200

    checkpoint

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,21 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const storedUser = localStorage.getItem('user');
+const parsedUser = storedUser ? JSON.parse(storedUser) : null;
+
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-        accessToken: undefined,
-        username: undefined,
-        id: undefined,
+        accessToken: localStorage.getItem('access') || undefined,
+        username: parsedUser?.username,
+        id: parsedUser?.id,
     },
     reducers: {
         login_user: (state, action) => {
             const { access, user } = action.payload;
-            // console.log(action.payload);
-            state.accessToken = action.payload.access;
+            state.accessToken = access;
             state.username = user.username;
             state.id = user.id;
             localStorage.setItem('access', access);
-            localStorage.setItem('user', user);
+            localStorage.setItem('user', JSON.stringify(user));
         },
         logout_user: (state) => {
             state.accessToken = null;
